673ecb5d34a26166b3e1f24301f484e1bf8e6335
Manikandan Ravikumar <manikandan.ravikumar@nosto.com>
2025-01-25 21:34:25 +0200

    eslint command

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,23 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 import { expressPlugin } from "./dev/server"
 
-export default defineConfig(({ mode }) => ({
-  plugins: [expressPlugin()],
-  build: {
-    minify: true,
-    sourcemap: true,
-    lib: {
-      name: "@nosto/nosto-web-components",
-      entry: [resolve(__dirname, "src/main.ts")],
-      formats: ["es", "cjs"],
-      fileName: (format, name) => `${name}.${format}.js`
+export default defineConfig(({ mode }) => {
+  console.log("mode: ", mode)
+  return {
+    plugins: [expressPlugin()],
+    build: {
+      emptyOutDir: true,
+      minify: true,
+      sourcemap: true,
+      lib: {
+        name: "@nosto/nosto-web-components",
+        entry: [resolve(__dirname, "src/main.ts")],
+        formats: ["es", "cjs"],
+        fileName: (format, name) => `${name}.${format}.js`
+      }
+    },
+    server: {
+      port: 8080
     }
-  },
-  server: {
-    port: 8080
   }
-}))
+})
