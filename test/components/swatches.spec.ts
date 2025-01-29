d21457b63a112f0a8b86fba16a58167acaddbf4d
Timo Westkämper <timo.westkamper@nosto.com>
2025-01-29 14:55:04 +0200

    Add select based swatch structure

--- test/components/swatches.spec.ts
+++ test/components/swatches.spec.ts
@@ -48,4 +48,44 @@ describe("swatch side effects", () => {
     // click on third size option should mark 145 as selected
     // atc available, since single SKU is selected
   })
+
+  it("should provide selection side effects on select element based swatches structure", () => {
+    element.setAttribute("product-id", "123")
+    element.setAttribute("reco-id", "789")
+
+    const mockAddSkuToCart = vi.fn()
+    window.Nosto = { addSkuToCart: mockAddSkuToCart }
+
+    element.innerHTML = `      
+      <select class="colors" n-swatch>
+        <!-- sku ids: 1** -->
+        <option n-skus="123,134,145" value="black">Black</option>
+        <!-- sku ids: 2** -->
+        <option n-skus="223,234,245" value="white">White</option>
+        <!-- sku ids: 3** -->
+        <option n-skus="323,334,345" value="blue">Blue</option>
+      </div>
+      <select class="sizes" n-swatch>
+        <!-- sku ids: **3 -->
+        <option n-skus="123,223,323" value="L">L</option>
+        <!-- sku ids: **4 -->
+        <option n-skus="134,234,334" value="M">M</option>
+        <!-- sku ids: **5 -->
+        <option n-skus="145,245,345" value="S">S</option>
+      </div>
+      <div n-atc>ATC</div>
+    `
+    element.connectedCallback()
+
+    // TODO assertions
+
+    // selection of first color option should mark 123,134,145 as selected
+    // atc not available, since multiple SKUs are selected
+
+    // selection of second size option should mark 134 as selected
+    // atc available, since single SKU is selected
+
+    // selection of third size option should mark 145 as selected
+    // atc available, since single SKU is selected
+  })
 })
