3df40285cc967ea2dceca17f8953bc8f0f9cc429
Timo Westkämper <timo.westkamper@nosto.com>
2025-02-04 10:44:36 +0200

    Simplify test

--- test/components/NostoSkuOptions.spec.ts
+++ test/components/NostoSkuOptions.spec.ts
@@ -4,6 +4,7 @@ import "@/components/NostoSkuOptions"
 import { NostoProduct } from "@/components/NostoProduct"
 
 type SkuOption = "colors" | "sizes"
+type SkuOptionValue = "black" | "white" | "blue" | "l" | "m" | "s"
 
 type OtherSkuOptions = {
   skuOption: SkuOption
@@ -20,33 +21,43 @@ describe("Sku options side effects", () => {
     nostoProduct = document.querySelector<NostoProduct>("nosto-product")!
   })
 
-  function testClickedSku(skuOption: SkuOption, skuItemIndex: number) {
-    const sourceElement = nostoProduct.querySelector(`nosto-sku-options[${skuOption}]`)
-    expect(sourceElement).toBeTruthy()
-    const skuOptionElement = sourceElement!.querySelector<HTMLElement>(`[n-option]:nth-child(${skuItemIndex})`)
-    expect(skuOptionElement).toBeTruthy()
-    skuOptionElement!.click()
-    expect(skuOptionElement!.hasAttribute("selected")).toBeTruthy()
+  function loadSkuContent() {
+    document.body.innerHTML = `
+      <nosto-product product-id="123" reco-id="789">
+        <nosto-sku-options colors>
+          <span black n-option n-skus="123,145" selected>Black</span>
+          <span white n-option n-skus="223,234,245">White</span>
+          <span blue n-option n-skus="334,345">Blue</span>
+        </nosto-sku-options>
+        <nosto-sku-options sizes>
+          <span l n-option n-skus="123,223">L</span>
+          <span m n-option n-skus="234,334">M</span>
+          <span s n-option n-skus="145,245,345">S</span>
+        </nosto-sku-options>
+      </nosto-product>
+    `
+  }
 
-    const otherSkuOptions = sourceElement!.querySelectorAll(`[n-option]:not(:nth-child(${skuItemIndex}))`)
-    expect(otherSkuOptions.length).toBeGreaterThan(0)
+  function element(value: SkuOptionValue) {
+    return nostoProduct.querySelector<HTMLElement>(`[${value}]`)!
+  }
+
+  function clickSkuOption(value: SkuOptionValue) {
+    element(value).click()
+  }
+
+  function verifySkuSelection(value: SkuOptionValue) {
+    const el = element(value)
+    expect(el.hasAttribute("selected")).toBeTruthy()
+    const otherSkuOptions = Array.from(element(value).parentNode!.children).filter(c => c !== el)
     const selectedOtherOptions = Array.from(otherSkuOptions).some(option => option.hasAttribute("selected"))
     expect(selectedOtherOptions).toBeFalsy()
   }
 
-  function testOtherSkusAfterClick({
-    skuOption,
-    visibleSkus = [],
-    hiddenSkus = [],
-    preSelected = []
-  }: OtherSkuOptions) {
+  function verifyOtherSkus({ skuOption, visibleSkus = [], hiddenSkus = [], preSelected = [] }: OtherSkuOptions) {
     const otherSkuElement = nostoProduct.querySelector(`nosto-sku-options[${skuOption}]`)
-    expect(otherSkuElement).toBeTruthy()
-
     testElementSkus(otherSkuElement!, visibleSkus, (element: HTMLElement) => element.style.display !== "none")
-
     testElementSkus(otherSkuElement!, hiddenSkus, (element: HTMLElement) => element.style.display === "none")
-
     testElementSkus(otherSkuElement!, preSelected, (element: HTMLElement) => element.hasAttribute("selected"))
   }
 
@@ -57,27 +68,10 @@ describe("Sku options side effects", () => {
       .every(optionElement => test(optionElement!))
   }
 
-  // TODO handle ATC
-  function loadSkuContent() {
-    document.body.innerHTML = `
-      <nosto-product product-id="123" reco-id="789">
-        <nosto-sku-options colors>
-          <span n-option n-skus="123,145" selected>Black</span>
-          <span n-option n-skus="223,234,245">White</span>
-          <span n-option n-skus="334,345">Blue</span>
-        </nosto-sku-options>
-        <nosto-sku-options sizes>
-          <span n-option n-skus="123,223">L</span>
-          <span n-option n-skus="234,334">M</span>
-          <span n-option n-skus="145,245,345">S</span>
-        </nosto-sku-options>
-      </nosto-product>
-    `
-  }
-
   it("selected skuId should be undefined when all SKU options are not selected", () => {
-    testClickedSku("colors", 2) // 223,234,245
-    testOtherSkusAfterClick({
+    clickSkuOption("white") // 223,234,245
+    verifySkuSelection("white")
+    verifyOtherSkus({
       skuOption: "sizes",
       visibleSkus: ["123,223", "234,334", "145,245,345"]
     })
@@ -86,14 +80,16 @@ describe("Sku options side effects", () => {
   })
 
   it("should provide selection side effects on SKU selection", () => {
-    testClickedSku("colors", 2) // 223,234,245
-    testOtherSkusAfterClick({
+    clickSkuOption("white") // 223,234,245
+    verifySkuSelection("white")
+    verifyOtherSkus({
       skuOption: "sizes",
       visibleSkus: ["123,223", "234,334", "145,245,345"]
     })
 
-    testClickedSku("sizes", 2)
-    testOtherSkusAfterClick({
+    clickSkuOption("m")
+    verifySkuSelection("m")
+    verifyOtherSkus({
       skuOption: "colors",
       visibleSkus: ["223,234,245", "334,345"],
       hiddenSkus: ["123,145"],
@@ -102,8 +98,9 @@ describe("Sku options side effects", () => {
 
     expect(nostoProduct.selectedSkuId).toBe("234")
 
-    testClickedSku("colors", 3) //334,345
-    testOtherSkusAfterClick({
+    clickSkuOption("blue") //334,345
+    verifySkuSelection("blue")
+    verifyOtherSkus({
       skuOption: "sizes",
       visibleSkus: ["234,334", "145,245,345"],
       hiddenSkus: ["123,223"],
