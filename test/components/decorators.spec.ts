e89db3ea6d5f9b827bdff9e9b38712cd13a6525a
Timo Westkämper <timo.westkamper@nosto.com>
2025-03-10 10:07:55 +0200

    Improve tests

--- test/components/decorators.spec.ts
+++ test/components/decorators.spec.ts
@@ -1,40 +1,47 @@
-import { describe, it, expect, vi } from "vitest"
-import { customElement } from "../../src/components/decorators"
+import { describe, it, expect, vi, beforeEach } from "vitest"
+import { customElement, attribute } from "../../src/components/decorators"
 
 describe("customElement", () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+    vi.spyOn(window.customElements, "define")
+  })
+
   it("should define a custom element if not already defined", () => {
     const tagName = "my-element"
     const constructor = class extends HTMLElement {}
 
-    // Mock the customElements registry
-    const defineSpy = vi.spyOn(window.customElements, "define")
-    const getSpy = vi.spyOn(window.customElements, "get").mockReturnValue(undefined)
-
     customElement(tagName)(constructor)
 
-    expect(getSpy).toHaveBeenCalledWith(tagName)
-    expect(defineSpy).toHaveBeenCalledWith(tagName, constructor)
-
-    // Restore the original implementation
-    defineSpy.mockRestore()
-    getSpy.mockRestore()
+    expect(window.customElements.define).toHaveBeenCalledWith(tagName, constructor)
   })
 
   it("should not redefine a custom element if already defined", () => {
     const tagName = "my-element"
     const constructor = class extends HTMLElement {}
 
-    // Mock the customElements registry
-    const defineSpy = vi.spyOn(window.customElements, "define")
-    const getSpy = vi.spyOn(window.customElements, "get").mockReturnValue(constructor)
-
     customElement(tagName)(constructor)
 
-    expect(getSpy).toHaveBeenCalledWith(tagName)
-    expect(defineSpy).not.toHaveBeenCalled()
+    expect(window.customElements.define).not.toHaveBeenCalled()
+  })
+})
 
-    // Restore the original implementation
-    defineSpy.mockRestore()
-    getSpy.mockRestore()
+@customElement("example-element")
+class Example extends HTMLElement {
+  @attribute("name")
+  accessor name!: string
+
+  @attribute("disabled", Boolean)
+  accessor disabled!: boolean
+}
+
+describe("attribute", () => {
+  it("should map attributes to properties", () => {
+    const example = new Example()
+    example.setAttribute("name", "John")
+    example.setAttribute("disabled", "")
+
+    expect(example.name).toBe("John")
+    expect(example.disabled).toBe(true)
   })
 })
