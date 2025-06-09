9383663f946bfe4c35d48e9b60ed434c9162162d
aistetamos <aiste@searchnode.com>
2025-06-09 12:10:06 +0300

    feat: add NostoCampaign component

---
+++ src/components/NostoCampaign.ts
@@ -0,0 +1,64 @@
+import { customElement } from "./decorators"
+import { nostojs } from "@nosto/nosto-js"
+
+@customElement("nosto-campaign")
+export class NostoCampaign extends HTMLElement {
+  static attributes = {
+    placement: String,
+    "div-id": String,
+    product: String,
+    variant: String
+  }
+
+  placement: string | undefined
+  product: string | undefined
+  variant: string | undefined
+
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.placement = this.getAttribute("placement") || this.getAttribute("div-id")!
+    this.product = this.getAttribute("product") ?? undefined
+    this.variant = this.getAttribute("variant") ?? undefined
+
+    if (!this.placement) {
+      console.error('<nosto-campaign> requires a "placement" or "div-id" attribute.')
+      return
+    }
+
+    this.loadCampaign()
+  }
+
+  async loadCampaign() {
+    try {
+      const api = await new Promise(nostojs)
+      const request = api
+        .createRecommendationRequest({ includeTagging: true })
+        .disableCampaignInjection()
+        .setElements([this.placement!])
+        .setResponseMode("HTML")
+
+      if (this.product) {
+        const product = { product_id: this.product }
+        request.setProducts([product])
+      }
+
+      if (this.variant) {
+        request.addCurrentVariation(this.variant)
+      }
+
+      const result = await request.load()
+      const html = result.recommendations[this.placement!]
+
+      if (html && typeof html === "string") {
+        this.innerHTML = html
+      } else {
+        console.warn(`No recommendation result for div ID: ${this.placement}`)
+      }
+    } catch (error) {
+      console.error("Failed to load campaign", error)
+    }
+  }
+}
