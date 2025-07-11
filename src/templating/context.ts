74a4ca39e6cb50f67eb3aa92c24b95d1d76e5528
Timo Westkämper <timo.westkamper@nosto.com>
2025-07-11 15:25:38 +0300

    feat: normalize context to camelCase
    BREAKING CHANGE: snake case to camel case context treatment

--- src/templating/context.ts
+++ src/templating/context.ts
@@ -13,9 +13,26 @@ function thumb(url: string, operations: Operations) {
   return url
 }
 
+function snakeToCamelCase(str: string) {
+  return str.replace(/_([a-z])/g, (_, l) => l.toUpperCase())
+}
+
+function toCamelCase<T>(context: T): T {
+  if (Array.isArray(context)) {
+    return context.map(toCamelCase) as T
+  } else if (context && typeof context === "object") {
+    return Object.fromEntries(
+      Object.entries(context).map(([key, value]) => {
+        return [snakeToCamelCase(key), toCamelCase(value)]
+      })
+    ) as T
+  }
+  return context
+}
+
 export function getContext(context: object) {
   return {
-    ...context,
+    ...toCamelCase(context),
     thumb,
     unpic
   }
--- test/templating/context.spec.ts
+++ test/templating/context.spec.ts
@@ -46,4 +46,26 @@ describe("getContext", () => {
       width: undefined
     })
   })
+
+  it("should convert object keys to camelCase", () => {
+    const context = {
+      some_key: "value",
+      nested_object: {
+        another_key: "another_value"
+      },
+      array_of_objects: [{ item_one: "one" }, { item_two: "two" }]
+    }
+
+    const result = getContext(context)
+
+    expect(result).toEqual({
+      someKey: "value",
+      nestedObject: {
+        anotherKey: "another_value"
+      },
+      arrayOfObjects: [{ itemOne: "one" }, { itemTwo: "two" }],
+      thumb: expect.any(Function),
+      unpic: expect.any(Function)
+    })
+  })
 })
