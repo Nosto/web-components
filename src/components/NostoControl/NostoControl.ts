6ef28f0bf063d5a232e6b6e2e7be4f1d4883227f
Timo Westkämper <timo.westkamper@nosto.com>
2025-07-17 16:06:56 +0300

    feat: create NostoControl for conditional rendering

---
+++ src/components/NostoControl/NostoControl.ts
@@ -0,0 +1,23 @@
+import { nostojs } from "@nosto/nosto-js"
+import { customElement } from "../decorators"
+import { NostoElement } from "../NostoElement"
+
+/**
+ * This component replaces its children with the content of the first template
+ * that matches the current search session segment.
+ */
+@customElement("nosto-control")
+export class NostoControl extends NostoElement {
+  async connectedCallback() {
+    const api = await new Promise(nostojs)
+    const { segments } = await api.getSearchSessionParams()
+    const template = Array.from(this.querySelectorAll<HTMLTemplateElement>(":scope > template[segment]")).find(el =>
+      // TODO provide more filtering options: schedule, affinities etc
+      segments?.includes(el.getAttribute("segment")!)
+    )
+    if (template) {
+      const clone = template.content.cloneNode(true) as DocumentFragment
+      this.replaceChildren(...clone.childNodes)
+    }
+  }
+}
