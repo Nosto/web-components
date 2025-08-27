a0c4b9accd945196e376ed875a9849bd3d2d967a
Timo Westkämper <timo.westkamper@nosto.com>
2025-08-27 11:05:47 +0300

    chore: fix deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,8 @@
 import type { StorybookConfig } from "@storybook/web-components-vite"
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-docs", "@storybook/addon-controls", "@storybook/addon-actions"],
+  stories: ["../src/**/*.stories.tsx"],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/web-components-vite",
     options: {}
