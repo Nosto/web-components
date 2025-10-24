3ff4d76fc27c2c4cfb477d9e88b1af81e1a72711
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-24 16:20:13 +0300

    chore: fix lint error

--- src/utils/createShopifyUrl.ts
+++ src/utils/createShopifyUrl.ts
@@ -1,6 +1,6 @@
 export function createShopifyUrl(url: string) {
   const root = new URL(window.Shopify?.routes?.root ?? "/", window.location.href).href
-    if (root.endsWith("/") && url.startsWith("/")) {
+  if (root.endsWith("/") && url.startsWith("/")) {
     return new URL(root + url.slice(1))
   }
   return new URL(root + url)
