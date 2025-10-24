d7f7171a8d761ed88ac72b2a61d69b97c18aface
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-24 16:11:42 +0300

    chore: use simpler URL joining

--- src/utils/createShopifyUrl.ts
+++ src/utils/createShopifyUrl.ts
@@ -1,11 +1,7 @@
 export function createShopifyUrl(url: string) {
   const root = new URL(window.Shopify?.routes?.root ?? "/", window.location.href).href
-  return join(root, url)
-}
-
-function join(base: string, url: string) {
-  if (url.startsWith("/")) {
-    url = url.slice(1)
+    if (root.endsWith("/") && url.startsWith("/")) {
+    return new URL(root + url.slice(1))
   }
-  return new URL(url, base)
+  return new URL(root + url)
 }
