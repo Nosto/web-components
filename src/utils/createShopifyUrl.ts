22514a06317462365f080114104782f34085986c
manikandan-ravikumar <manikandan.ravikumar@nosto.com>
2025-11-21 14:30:09 +0200

    fix: simplify resolveUrl

--- src/utils/createShopifyUrl.ts
+++ src/utils/createShopifyUrl.ts
@@ -8,10 +8,5 @@ export function createShopifyUrl(url: string) {
 
 // Resolves the root URL of the Shopify store and excludes any query parameters.
 function resolveRootUrl() {
-  // Specific clause to handle design mode (Theme Editor mode) in Shopify admin
-  // Shopify adds oseid parameter on href/toString invocation on URL interface
-  if (window.Shopify?.designMode) {
-    return `${window.location.origin}${window.Shopify?.routes?.root ?? "/"}`
-  }
-  return new URL(window.Shopify?.routes?.root ?? "/", window.location.origin).href
+  return `${window.location.origin}${window.Shopify?.routes?.root ?? "/"}`
 }
