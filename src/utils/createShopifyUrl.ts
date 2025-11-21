cf2eb435e7d184d156fd94afe119d7aed5578a09
manikandan-ravikumar <manikandan.ravikumar@nosto.com>
2025-11-21 09:47:17 +0200

    fix: update createShopifyUrl to handle query params in URL

--- src/utils/createShopifyUrl.ts
+++ src/utils/createShopifyUrl.ts
@@ -1,7 +1,12 @@
 export function createShopifyUrl(url: string) {
-  const root = new URL(window.Shopify?.routes?.root ?? "/", window.location.href).href
+  const root = resolveRootUrl()
   if (root.endsWith("/") && url.startsWith("/")) {
     return new URL(root + url.slice(1))
   }
   return new URL(root + url)
+}
+
+// Resolves the root URL of the Shopify store and excludes any query parameters.
+function resolveRootUrl() {
+  return new URL(window.Shopify?.routes?.root ?? "/", window.location.origin).href
 }
