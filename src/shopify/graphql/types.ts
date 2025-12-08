8c51130c1a200b464faf930c53029f4c12ee3b89
manikandan-ravikumar <manikandan.ravikumar@nosto.com>
2025-12-08 18:51:56 +0200

    chore: move types back to it's original location

---
+++ src/shopify/graphql/types.ts
@@ -0,0 +1,39 @@
+import { ProductByHandleQuery } from "./generated/storefront.generated"
+
+export type GraphQLResponse<T> = {
+  data: T
+}
+
+export type GraphQLProduct = NonNullable<ProductByHandleQuery["product"]>
+
+// Derive flattened types directly from the generated GraphQL types
+export type ShopifyImage = GraphQLProduct["images"]["nodes"][0]
+export type ShopifyMoney = GraphQLProduct["adjacentVariants"][0]["price"]
+export type ShopifySelectedOption = GraphQLProduct["adjacentVariants"][0]["selectedOptions"][0]
+
+export type ShopifyVariant = GraphQLProduct["adjacentVariants"][0]
+
+export type ShopifyOptionValue = GraphQLProduct["options"][0]["optionValues"][0]
+
+export type ShopifyOption = {
+  name: GraphQLProduct["options"][0]["name"]
+  optionValues: ShopifyOptionValue[]
+}
+
+// Flattened product type derived from GraphQL with augmented fields
+// TODO: Omit adjacentVariants later and rename variants to combinedVariants
+export type ShopifyProduct = Omit<GraphQLProduct, "images" | "options"> & {
+  images: ShopifyImage[]
+  options: ShopifyOption[]
+  // augmented fields
+  price: ShopifyMoney
+  compareAtPrice: ShopifyMoney | null
+  variants: ShopifyVariant[]
+}
+
+/**
+ * Event detail for variant change events
+ */
+export type VariantChangeDetail = {
+  variant: ShopifyVariant
+}
